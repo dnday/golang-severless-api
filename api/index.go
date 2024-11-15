4ac837a094c5d0cb0a950088cda0ee49827d21a3
dnday <marceldino9@gmail.com>
2024-11-15 11:34:58 +0700

    initialize severless API

---
+++ api/index.go
@@ -0,0 +1,9 @@
+package handler
+
+import (
+	"net/http"
+)
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dnday/golang-serverless
+
+go 1.23.1
